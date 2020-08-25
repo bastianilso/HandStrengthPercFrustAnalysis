d76f1ec5141cd7acddf990cf919346883edb59bd
Bastian Ilso <contact@bastianilso.com>
2020-08-25 15:54:35 +0200

    Update Scripts

--- PercFrustDraft.R
+++ PercFrustDraft.R
@@ -2,67 +2,218 @@ library(gsheet)
 library(dplyr)
 library(plotly)
 
+# Interaction features
+# - Toggle, ColorSize=Delta
+# - Toggle, Color=Condition
+# - Jitter, Amount
+
+# How to export plots to PDF from Plotly
+orca(fig, "surface-plot.pdf", width=350, height=350)
+
+# How to export HTML plots from Plotly
+htmlwidgets::saveWidget(config(p, displayModeBar = FALSE), "graph.html")
+
+#######################
+# LOAD AND SETUP DATA #
+#######################
+
+# Load data from participant surveys.
 url <- 'https://docs.google.com/spreadsheets/d/1k_bkdEwiXWoO5sZmxpKthuySS7T7151qzfQJ_SY7fdI/edit#gid=2137346269'
 D <- gsheet2tbl(url)
+
+# Load recognition rates, as logged by the game.
+load("data_rates.rda")
+
+# PID as factor.
+D_rates <- D_rates %>%
+  mutate(PID = as.factor(PID))
 
 D_valid <- D %>%
   filter(!is.na(Frustration)) %>%
   mutate(Participant = as.factor(Participant),
          Condition = as.character(Condition))
+         
+# Version A: Assume that each condition had the actual rate it has.
+#D_valid$PureControl[D_valid$Condition == "0"] = 0.50
+#D_valid$PureControl[D_valid$Condition == "100"] = 1
+#D_valid$FabControl[D_valid$Condition == "15"] = 0.65
+#D_valid$FabControl[D_valid$Condition == "30"] = 0.80
+#D_valid$FabControl[D_valid$Condition == "50"] = 1
 
-D_valid$PureControl[D_valid$Condition == "0"] = 0.50
-D_valid$PureControl[D_valid$Condition == "100"] = 1
-D_valid$FabControl[D_valid$Condition == "15"] = 0.65
-D_valid$FabControl[D_valid$Condition == "30"] = 0.80
-D_valid$FabControl[D_valid$Condition == "50"] = 1
+# Version B: Use calculations based on what actually happened in terms of task-outcomes.
+D_real <- D_valid %>%
+  inner_join(D_rates, by = c("Participant" = "PID", "Order" = "ConditionOrder"))
 
-D_R0 = D_valid %>% filter(Condition == "0")
-D_R100 = D_valid %>% filter(Condition == "100")
-D_15 = D_valid %>% filter(Condition == "15")
-D_30 = D_valid %>% filter(Condition == "30")
-D_50 = D_valid %>% filter(Condition == "50")
+# Exclude trials that were bad 
+
+D_R0 = D_real %>% filter(Condition == "0", Recog.Rate == 0.5)
+D_R100 = D_real %>% filter(Condition == "100", Recog.Rate == 1)
+D_15 = D_real %>% filter(Condition == "15", Recog.Rate == 0.5, Fab.Rate == 0.15)
+D_30 = D_real %>% filter(Condition == "30", Recog.Rate == 0.5, Fab.Rate == 0.30)
+D_50 = D_real %>% filter(Condition == "50", Recog.Rate == 0.5, Fab.Rate == 0.50)
+
+D_comb <- bind_rows(D_R0, D_R100, D_15, D_30, D_50)
+
+excludes <- c(135, 164, 165, 212, 243, 245)
+
+######################
+# VERIFICATION PLOTS #
+######################
+
+# Check recognition rates across participants
+D_comb %>%
+  plot_ly() %>%
+  add_trace(x=~Participant, y=~FabRecogRate, mode='markers', type='scatter')
+
+# Check to what extent trying one condition after another has an impact.
+D_comb <- D_comb %>% group_by(Participant) %>%
+  mutate(ConditionLag = lag(FabRecogRate))%>%
+  rowwise() %>%
+  mutate(ConditionDelta = if (!is.na(ConditionLag)) FabRecogRate-ConditionLag else NA)
+
+
+# Plot all ratings and see what happens when people score 100 compared to fab15%.
+D_comb$tm <- 1:nrow(D_comb)
+fig <- D_comb %>%
+  plot_ly() %>%
+  group_by(Participant) %>%
+  add_trace(x=~tm, y=~PerceivedControl, mode='lines+markers', type='scatter', color=I('gray')) %>%
+  filter(Condition == '100') %>%
+  add_trace(x=~tm, y=~PerceivedControl, mode='markers', type='scatter', color=I('black'))
+fig <- fig %>%
+  add_trace(x=Dcomb30$tm, y=Dcomb30$PerceivedControl, mode='markers', type='scatter', color=I('SkyBlue'))
+fig
+
+
+###################################
+# PERCEIVED CONTROL - FRUSTRATION #
+###################################
+
+# Check that each condition has proper rates.
+D_comb %>%
+  plot_ly() %>%
+  add_trace(x=~Condition, y=~FabRecogRate, mode='markers', type='scatter')
 
 
 # Plot Level of Control vs Frustration Level
-FrustPercCurve.All = supsmu(D_valid$PerceivedControl, D_valid$Frustration)
-FrustPercCurve.15 = supsmu(D_15$PerceivedControl, D_15$Frustration)
-FrustPercCurve.30 = supsmu(D_30$PerceivedControl, D_30$Frustration)
-FrustPercCurve.50 = supsmu(D_50$PerceivedControl, D_50$Frustration)
-FrustPercCurve.R100 = supsmu(D_R100$PerceivedControl, D_R100$Frustration)
-FrustPercCurve.R0 = supsmu(D_R0$PerceivedControl, D_R0$Frustration)
+FrustPercCurve.All = supsmu(D_comb$PercNormalized, D_comb$FrustNormalized)
+#FrustPercCurve.15 = supsmu(D_15$PerceivedControl, D_15$Frustration)
+#FrustPercCurve.30 = supsmu(D_30$PerceivedControl, D_30$Frustration)
+#FrustPercCurve.50 = supsmu(D_50$PerceivedControl, D_50$Frustration)
+#FrustPercCurve.R100 = supsmu(D_R100$PerceivedControl, D_R100$Frustration)
+#FrustPercCurve.R0 = supsmu(D_R0$PerceivedControl, D_R0$Frustration)
+
+qfit15 <- lm(Frustration ~ PerceivedControl, data = D_15) 
+qfit30 <- lm(Frustration ~ PerceivedControl, data = D_30) 
+qfit50 <- lm(Frustration ~ PerceivedControl, data = D_50) 
+qfitR100 <- lm(Frustration ~ PerceivedControl, data = D_R100) 
+qfitR0 <- lm(Frustration ~ PerceivedControl, data = D_R0) 
+FrustPercCurve.15 <- data.frame(x = (1:100)/100, y = predict(qfit15, data.frame(PerceivedControl = (1:100)/100)))
+FrustPercCurve.30 <- data.frame(x = (1:100)/100, y = predict(qfit30, data.frame(PerceivedControl = (1:100)/100)))
+FrustPercCurve.50 <- data.frame(x = (1:100)/100, y = predict(qfit50, data.frame(PerceivedControl = (1:100)/100)))
+FrustPercCurve.R100 <- data.frame(x = (1:100)/100, y = predict(qfitR100, data.frame(PerceivedControl = (1:100)/100)))
+FrustPercCurve.R0 <- data.frame(x = (1:100)/100, y = predict(qfitR0, data.frame(PerceivedControl = (1:100)/100)))
+
+
 col = c('Tan', 'Maroon', 'Peru', 'Chocolate', 'SaddleBrown')
 
-D_valid %>%
-  mutate(PerceivedControl = jitter(PerceivedControl, amount=.03),
-         Frustration = jitter(Frustration, amount=.03)) %>%
+D_comb %>%
+  mutate(PerceivedControl = jitter(PercNormalized, amount=.02),
+         Frustration = jitter(FrustNormalized, amount=.02)) %>%
+  arrange(Participant, Order) %>%
   plot_ly() %>%
-  add_trace(x=~PerceivedControl, y=~Frustration, color=~Condition, colors=col, mode='markers') %>%
-  add_trace(name="Smooth Line", x=FrustPercCurve.All$x, y=FrustPercCurve.All$y, mode='lines')
+  add_trace(x=~PerceivedControl, y=~Frustration, color=I('Gray'), mode='markers') %>%
+  add_trace(x=~FrustPercCurve.All$x, y=~FrustPercCurve.All$y, color=I('Black'), mode='lines+markers') %>%
+  #add_trace(x=~PercNormalized, y=~FrustNormalized, mode='markers+lines', color=~Participant, size=~as.numeric(Condition), transforms = list(type = 'groupby',groups = ~Condition)) %>%
   #add_trace(name="15% Fab", x=FrustPercCurve.15$x, y=FrustPercCurve.15$y, mode='lines', color=I('Peru')) %>%
   #add_trace(name="30% Fab", x=FrustPercCurve.30$x, y=FrustPercCurve.30$y, mode='lines', color=I('Chocolate')) %>%
   #add_trace(name="50% Fab", x=FrustPercCurve.50$x, y=FrustPercCurve.50$y, mode='lines', color=I('SaddleBrown')) %>%
   #add_trace(name="100% Recog", x=FrustPercCurve.R100$x, y=FrustPercCurve.R100$y, mode='lines', color=I('Maroon')) %>%
-  #add_trace(name="0% Fab", x=FrustPercCurve.R0$x, y=FrustPercCurve.R0$y, mode='lines',color=I('Tan'))
+  #add_trace(name="0% Fab", x=FrustPercCurve.R0$x, y=FrustPercCurve.R0$y, mode='lines',color=I('Tan')) %>%
+  layout(xaxis = list(range=c(0,1.1)), yaxis = list(range=c(0,1.1), scaleanchor = "x"))
+orca(fig, "hand-perceived-control-frustration.pdf", width=350, height=350)
+  
+#################################
+# CALCULATE TASK-OUTCOME RATES  #
+#################################
+D_comb = D_comb %>%
+  rowwise() %>%
+  mutate(PureControl = if (Fab.Rate == 0) Recog.Rate else NA,
+         FabControl = if (Fab.Rate > 0) Fab.Rate + Recog.Rate else NA)
 
 
 # Plot Level of Control vs Perceived Control
-PercPureCurve = supsmu(D_valid$PureControl, D_valid$PerceivedControl)
-PercFabCurve = supsmu(D_valid$FabControl, D_valid$PerceivedControl)
-D_valid %>%
+PercPureCurve = supsmu(D_comb$PureControl, D_comb$PerceivedControl)
+PercPureCurve$x <- c(0,100)
+PercPureCurve$x <- as.factor(PercPureCurve$x)
+PercFabCurve = supsmu(D_comb$FabControl, D_comb$PerceivedControl)
+PercFabCurve$x <- c(15,30,50)
+PercFabCurve$x <- as.factor(PercFabCurve$x)
+fig1 <-D_comb %>%
   mutate(PerceivedControl = jitter(PerceivedControl, amount=.03),
          PureControl = jitter(PureControl, amount=.03),
          FabControl = jitter(FabControl, amount=.03)) %>%
   plot_ly() %>%
-  add_trace(name="Pure Control", x=~PureControl, y=~PerceivedControl, color=I('LightSkyBlue')) %>%
-  add_trace(name="Fab Control", x=~FabControl, y=~PerceivedControl, color=I('LightSalmon')) %>%
+  add_trace(type='scatter', mode='markers', name="Pure Control", x=~PureControl, y=~PerceivedControl, color=I('darkgray'), symbol=I('circle')) %>%
+  add_trace(type='scatter', mode='markers', name="Fab Control", x=~FabControl, y=~PerceivedControl, color=I('gray'),symbol=I('circle-open')) %>%
+  add_trace(type='scatter', name="Pure Control", x=PercPureCurve$x, y=PercPureCurve$y, mode='lines+markers', color=I('Black'), symbol=I('circle')) %>%
+  add_trace(type='scatter', name="Fab Control", x=PercFabCurve$x, y=PercFabCurve$y, mode='lines+markers', color=I('Black'), line=list(dash='dashdot')) %>%
+  layout(xaxis = list(range=c(0,1.1), title="Level of Control"), yaxis = list(range=c(0,1.1),scaleanchor="x"),showlegend=FALSE)
+orca(fig1, "hand-level-of-control-perceived-control.pdf", width=350, height=350)
+
+D_comb$Condition <- as.factor(D_comb$Condition)
+
+# Plot Level of Control vs Frustration
+FrustPureCurve = supsmu(D_comb$PureControl, D_comb$Frustration)
+FrustFabCurve = supsmu(D_comb$FabControl, D_comb$Frustration)
+
+fig <- D_comb %>%
+  mutate(Frustration = jitter(Frustration, amount=.03),
+         PureControl = jitter(PureControl, amount=.03),
+         FabControl = jitter(FabControl, amount=.03)) %>%
+  plot_ly() %>%
+  add_trace(x=~PureControl, name="Pure Control", y=~Frustration, color=I('darkgray'), symbol=I('circle')) %>%
+  add_trace(x=~FabControl, name="Fab Control", y=~Frustration, color=I('gray'), symbol=I('circle-open')) %>%
+  add_trace(name="Pure Control", x=FrustPureCurve$x, y=FrustPureCurve$y, mode='lines+markers', color=I('black')) %>%
+  add_trace(name="Fab Control", x=FrustFabCurve$x, y=FrustFabCurve$y, mode='lines+markers', color=I('black'), line=list(dash='dashdot')) %>%
+  layout(xaxis = list(title="Level of Control", range=c(0,1.1)), yaxis = list(range=c(0,1.1), scaleanchor="x"), showlegend=FALSE)
+orca(fig, "hand-level-of-control-frustration.pdf", width=350, height=350)
+
+###################################
+# CALCULATE ACTION-OUTCOME RATES  #
+###################################
+
+load("data_blinkrate.rda")
+
+D_blinkrates <- D_blinkrates %>%
+  mutate(PID = as.factor(PID))
+
+D_comb <- D_comb %>%
+  inner_join(D_blinkrates, by = c("Participant" = "PID", "Order" = "ConditionOrder") )
+
+#BlinkCount Verification
+D_comb %>%
+  plot_ly() %>%
+  add_trace(x=~FabRecogRate, y=~BlinkCount, hoverinfo='text', hovertext=paste('BlinkCount:',D_comb$BlinkCount), mode='markers', type='scatter')
+
+# Plot Level of Control vs Perceived Control
+PercPureCurve = supsmu(D_comb$PureControl, D_comb$PerceivedControl)
+PercFabCurve = supsmu(D_comb$FabControl, D_comb$PerceivedControl)
+D_comb %>%
+  mutate(PerceivedControl = jitter(PerceivedControl, amount=.03),
+         PureControl = jitter(PureControl, amount=.03),
+         FabControl = jitter(FabControl, amount=.03)) %>%
+  plot_ly() %>%
+  add_trace(name="Pure Control", x=~PureControl, y=~PerceivedControl) %>%
+  add_trace(name="Fab Control", x=~FabControl, y=~PerceivedControl) %>%
   add_trace(name="Pure Control", x=PercPureCurve$x, y=PercPureCurve$y, mode='lines+markers', color=I('SkyBlue')) %>%
   add_trace(name="Fab Control", x=PercFabCurve$x, y=PercFabCurve$y, mode='lines+markers', color=I('Salmon')) %>%
   layout(xaxis = list(title="Level of Control", range=c(0,1.1)), yaxis = list(range=c(0,1.1)))
 
 # Plot Level of Control vs Frustration
-FrustPureCurve = supsmu(D_valid$PureControl, D_valid$Frustration)
-FrustFabCurve = supsmu(D_valid$FabControl, D_valid$Frustration)
-D_valid %>%
+FrustPureCurve = supsmu(D_comb$PureControl, D_comb$Frustration)
+FrustFabCurve = supsmu(D_comb$FabControl, D_comb$Frustration)
+D_comb %>%
   mutate(Frustration = jitter(Frustration, amount=.03),
          PureControl = jitter(PureControl, amount=.03),
          FabControl = jitter(FabControl, amount=.03)) %>%
---
+++ PercFrustStats.R
@@ -0,0 +1,209 @@
+library(gsheet)
+library(dplyr)
+library(plotly)
+library(coin)
+require(lme4) # For ordinal logistic regression
+library(ggplot2)
+library(Rmisc) #For SummarySE function to get error bars
+source("friedman.with.post.hoc.R")
+
+#############
+# GOALS ETC #
+#############
+
+# Game Frame: Run T-test with kiwi versus hand-strengthener, see if there are differences (?)
+# In relation to game frame, we observe higher frustrations at lower perceived levels of control.
+
+
+# Verify that we get similar kinds of curves and possibly compare them..
+
+# Do we have a impact from giving people 100% on peoples rating?
+
+# Check the counter-balancing, verify that it exists, and if not, how we can .... ??? make it so
+
+# Plot average delay vs perceived control ratings, and compare to Greville.
+
+
+
+
+# Motivation for separate study:
+# - Include extra levels, to "complete the curve" and provide a better picture of its development.
+# - Include a level with 100% recognition to provide some initial data on the corresponding "recognition curve", to contrast to the "fabrication curve"
+
+
+#######################
+# LOAD AND SETUP DATA #
+#######################
+
+url <- 'https://docs.google.com/spreadsheets/d/1k_bkdEwiXWoO5sZmxpKthuySS7T7151qzfQJ_SY7fdI/edit#gid=2137346269'
+D <- gsheet2tbl(url)
+
+# Load recognition rates, as logged by the game.
+load("data_rates.rda")
+
+D_rates <- D_rates %>%
+  mutate(PID = as.factor(PID))
+
+D_valid <- D %>%
+  filter(!is.na(Frustration)) %>%
+  mutate(Participant = as.factor(Participant),
+         Condition = as.character(Condition))
+
+D <- D_valid %>%
+  inner_join(D_rates, by = c("Participant" = "PID", "Order" = "ConditionOrder"))
+
+# Exclude trials that were bad 
+D = bind_rows(D %>% filter(Condition == "0", Recog.Rate == 0.5, Fab.Rate == 0.0),
+              D %>% filter(Condition == "100", Recog.Rate == 1, Fab.Rate == 0.0),
+              D %>% filter(Condition == "15", Recog.Rate == 0.5, Fab.Rate == 0.15),
+              D %>% filter(Condition == "30", Recog.Rate == 0.5, Fab.Rate == 0.30),
+              D %>% filter(Condition == "50", Recog.Rate == 0.5, Fab.Rate == 0.50))
+
+# Exclude participants who lack trials?
+D %>%
+  plot_ly() %>%
+  add_trace(x=~Participant, y=~Condition)
+excludes <- c("1","13","16","17","21", "24")
+
+D_excl <- D %>% filter(!Participant %in% excludes)
+
+
+########################################
+# LINEAR MODEL CONDITION WITHOUT ORDER #
+########################################
+#first check with Linear model effect for sham rate but not order
+D_excl$Condition = as.factor(D_excl$Condition)
+D_excl = D_excl %>% arrange(Condition)
+D_excl$conditionCont<-as.numeric(as.factor(D_excl$Condition))
+# Question: Do I need to filter out the 100% Condition?
+summary(lm(FrustrationEpisode~Condition,data=D_excl %>% filter(Condition != "100")))
+
+#################################################
+# SPLIT-UP LINEAR MODEL CONDITION WITHOUT ORDER #
+#################################################
+# In this version we split the dataset in two linear models - one modelling 0,15,30,50 sham, another modeling 0,100 true recognition.
+D_fab = D %>% filter(Condition != 100)
+D_rec = D %>% filter(Condition %in% c("0","100"))
+
+# Question: Why are we using the conditionCont variable instead of just using Condition?
+summary(lm(PerceivedControlEpisode~Condition,data=D_fab))
+summary(lm(PerceivedControlEpisode~Condition,data=D_rec))
+
+# Question: Why are we not doing the same for Frustration?
+summary(lm(FrustrationEpisode~Condition,data=D_fab))
+summary(lm(FrustrationEpisode~Condition,data=D_rec))
+
+#####################################
+# LINEAR MODEL CONDITION WITH ORDER #
+#####################################
+#first check with Linear model effect with shamChange
+D = D %>% arrange(Participant,Order)
+D = D %>% group_by(Participant) %>% mutate(ConditionChange = conditionCont-lag(conditionCont))
+D[is.na(D$ConditionChange),]$ConditionChange<-0
+summary(step(lm(PerceivedControlEpisode~ConditionChange,data=D)))
+
+## stats analyis modelling  -------------
+summary(step(lm(PerceivedControlEpisode~ConditionChange,data=D)))
+
+###############################
+# FRIEDMAN TEST WITH POST HOC # # NOT WORKING DUE TO 'SYMMETRY PROBLEM'
+###############################
+# NB: FRIEDMAN TESTS REQUIRE DATA FRAME OBJECT TYPES, CANNOT PARSE TIBBLE FACTORS!!!
+
+# https://www.r-statistics.com/2010/02/post-hoc-analysis-for-friedmans-test-r-code/
+
+
+#friedman.test.with.post.hoc(FrustrationEpisode ~ as.factor(Condition) | as.factor(Partcipant), D_excl %>% )
+D_fry <- D_excl %>% dplyr::select(FrustrationEpisode, Condition, Participant) %>% filter(Condition != "100")
+D_fry <- as.data.frame(D_fry)
+D_fry$Condition = as.factor(D_fry$Condition)
+str(D_fry)
+friedman.test(FrustrationEpisode~Condition|Participant, D_fry)
+
+
+
+#D_excl %>% dplyr::select(FrustrationEpisode, Condition, Participant) %>% filter(Condition != "100") %>% View()
+
+#D_excl <- D_excl %>% group_by(Participant) %>%
+#  mutate(Pattern = paste(Condition, collapse='-'))
+
+#D_excl %>% dplyr::select(Pattern) %>% group_by(Pattern) %>%
+#  summarize(cond = length(Pattern))
+
+#t <- D_excl %>% dplyr::select(FrustrationEpisode, Condition, Participant)
+#isSymmetric(t)
+
+#D %>% plot_ly() %>%
+#  add_trace(x=~Condition, y=~Count())
+
+#symmetry_test(FrustrationEpisode ~ as.factor(Condition) | as.factor(Participant), data = D_excl)
+#obj <- friedman_test(as.numeric(FrustrationEpisode) ~ as.factor(Condition) | as.factor(Participant), data = D_excl)
+
+#################
+# WILCOXON TEST #
+#################
+
+D_wil <- D_excl %>% dplyr::select(FrustrationEpisode,PerceivedControlEpisode, Condition, Participant) %>% filter(Condition != "100")
+
+wilcoxsign_test(D_wil[D_wil$Condition=='0',]$FrustrationEpisode ~ D_wil[D_wil$Condition=='15',]$FrustrationEpisode, distribution="exact")
+wilcoxsign_test(D_wil[D_wil$Condition=='0',]$FrustrationEpisode ~ D_wil[D_wil$Condition=='30',]$FrustrationEpisode, distribution="exact")
+wilcoxsign_test(D_wil[D_wil$Condition=='0',]$FrustrationEpisode ~ D_wil[D_wil$Condition=='50',]$FrustrationEpisode, distribution="exact")
+wilcoxsign_test(D_wil[D_wil$Condition=='15',]$FrustrationEpisode ~ D_wil[D_wil$Condition=='30',]$FrustrationEpisode, distribution="exact")
+wilcoxsign_test(D_wil[D_wil$Condition=='15',]$FrustrationEpisode ~ D_wil[D_wil$Condition=='50',]$FrustrationEpisode, distribution="exact")
+wilcoxsign_test(D_wil[D_wil$Condition=='30',]$FrustrationEpisode ~ D_wil[D_wil$Condition=='50',]$FrustrationEpisode, distribution="exact")
+wilcoxsign_test(GroupA ~ GroupB, distribution="exact")
+wilcoxsign_test(D_wil[D_wil$Condition=='0',]$PerceivedControlEpisode ~ D_wil[D_wil$Condition=='15',]$PerceivedControlEpisode, distribution="exact")
+wilcoxsign_test(D_wil[D_wil$Condition=='0',]$PerceivedControlEpisode ~ D_wil[D_wil$Condition=='30',]$PerceivedControlEpisode, distribution="exact")
+wilcoxsign_test(D_wil[D_wil$Condition=='0',]$PerceivedControlEpisode ~ D_wil[D_wil$Condition=='50',]$PerceivedControlEpisode, distribution="exact")
+wilcoxsign_test(D_wil[D_wil$Condition=='15',]$PerceivedControlEpisode ~ D_wil[D_wil$Condition=='30',]$PerceivedControlEpisode, distribution="exact")
+wilcoxsign_test(D_wil[D_wil$Condition=='30',]$PerceivedControlEpisode ~ D_wil[D_wil$Condition=='50',]$PerceivedControlEpisode, distribution="exact")
+wilcoxsign_test(D_wil[D_wil$Condition=='30',]$PerceivedControlEpisode ~ D_wil[D_wil$Condition=='50',]$PerceivedControlEpisode, distribution="exact")
+
+
+
+###############################
+# ORDINAL LOGISTIC REGRESSION #
+###############################
+
+#check with ordinal logistic reqression - no significance?
+summary(polr(as.factor(FrustrationEpisode) ~ Condition, data = D, Hess=TRUE))
+
+percContr.null<- lmer(PerceivedControlEpisode~(1|Participant),data=D,REML=FALSE)
+percContr.Condition <-lmer(PerceivedControlEpisode~Condition+(1|Participant),data=D,REML=FALSE)
+percContr.ConditionChange <-lmer(PerceivedControlEpisode~ConditionChange+(1|Participant),data=D,REML=FALSE)
+anova(percContr.null,percContr.ConditionChange)
+anova(percContr.null,percContr.Condition)
+percContr.ConditionOrder <-lmer(PerceivedControlEpisode~Condition+ConditionChange+(1|Participant),data=D,REML=FALSE)
+anova(percContr.Condition,percContr.ConditionOrder)
+
+# lmer for frustration
+Frust.null<- lmer(FrustrationEpisode~(1|Participant),data=D,REML=FALSE)
+Frust.Condition <-lmer(FrustrationEpisode~Condition+(1|Participant),data=D,REML=FALSE)
+anova(Frust.null,Frust.Condition)
+FrustContr.ConditionOrder <-lmer(FrustrationEpisode~Condition+ConditionChange+(1|Participant),data=D,REML=FALSE)
+# friedmann test on sham amount
+anova(Frust.Condition,FrustContr.ConditionOrder)
+
+####################################################
+# CHECK IF PERC. CONTROL IS INVERSE OF FRUSTRATION #
+####################################################
+
+#check if perceived control is just inverse of Frustration..
+summary(step(lm(FrustrationEpisode~PerceivedControlEpisode*as.factor(Condition),D)))
+
+###########
+# GGPLOTS #
+###########
+
+# plotting fabrictaion Rate vs Frustration w/ Error Bars (95% Confidence Interval)  ----------- 
+# Plot Sham Rate vs Frustration w/ Error Bars (95% Confidence Interval)
+dsm = summarySE(D, measurevar="Frustration", groupvars=c("Condition"))
+ggplot(dsm, aes(x=dsm$Condition, y=dsm$Frustration)) +
+  geom_errorbar(aes(ymin=dsm$Frustration-dsm$ci, ymax=dsm$F+dsm$ci), width=.1, size=.5) +
+  geom_point(size=3.25) +
+  geom_line(aes(group=1), size=1.25) +
+  ylim(0,1) +
+  theme_bw() +
+  theme(text = element_text(size = 13)) +
+  xlab("Fabrication Rate (%)") +
+  ylab("Frustration")
